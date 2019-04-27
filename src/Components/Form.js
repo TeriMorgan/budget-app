94342f559b9e6e7598a17be0d5683c10ecdcfa4d
Teri Morgan <terimorgan@Teris-MacBook-Pro.local>
2019-04-26 20:31:47 -0600

    post Chris

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,85 +1,101 @@
-import React, { Component } from 'react'
-import Logo from './Logo';
-import TransactionDateInput from './TransactionDateInput';
-import Select from './Select';
-import axios from 'axios';
+import React, { Component } from "react";
+import Logo from "./Logo";
+import TransactionDateInput from "./TransactionDateInput";
+import Select from "./Select";
+import axios from "axios";
 
-const API_SAVE_TRANSACTION  = 'http://terimorgan.com/api/contact/divvy-app/saveTransaction.php';
+const API_SAVE_TRANSACTION =
+  "http://terimorgan.com/api/contact/divvy-app/saveTransaction.php";
 
 class Form extends Component {
   constructor(props) {
-    super(props)
-  
+    super(props);
+
     this.state = {
-       amount: "",
-       date: "",
-       cat_id: ""
-    }
+      amount: "",
+      date: "",
+      cat_id: ""
+    };
   }
 
   handleSubmit = event => {
     axios({
-      method: 'post',
+      method: "post",
       url: `${API_SAVE_TRANSACTION}`,
-      headers: {'content-type': 'application/json'},
+      headers: { "content-type": "application/json" },
       data: this.state
     })
-    .then(result => {                                  //Do I need to do something with result?      
+      .then(result => {
+        //Do I need to do something with result?
         this.clearState();
-    })
-    .catch(ex => {
-      alert("The category cannot be saved.");
-      console.warn(ex);
-    });
-  }
+      })
+      .catch(ex => {
+        alert("The category cannot be saved.");
+        console.warn(ex);
+      });
+  };
 
   handleTransactionDate = TransDateInputState => {
     console.log(TransDateInputState);
-    this.setState({
-      amount: TransDateInputState.amount,
-      date: TransDateInputState.date
-    }, 
-    () => console.log(this.state));
-  }
+    this.setState(
+      {
+        amount: TransDateInputState.amount,
+        date: TransDateInputState.date
+      },
+      () => console.log(this.state)
+    );
+  };
 
-  handleCatId = SelectState => {
-    console.log(SelectState);
-    this.setState({
-      cat_id: SelectState.selectedCategoryId
-    },
-    () => console.log(this.state));
-  }
+  // handleCatId = SelectState => {
+  //   console.log(SelectState);
+  //   this.setState({
+  //     cat_id: SelectState.selectedCategoryId
+  //   },
+  //   () => console.log(this.state));
+  // }
+
+  handleCatId = selectedCategoryId => {
+    console.log(selectedCategoryId);
+    this.setState(
+      {
+        cat_id: selectedCategoryId
+      },
+      () => console.log(this.state)
+    );
+  };
 
   clearState = () => {
-    this.setState({
-      amount: "",
-      date: "",
-      cat_id: ""
-    }, () => {
-      console.log(this.state)
-    })}
-  
+    this.setState(
+      {
+        amount: "",
+        date: "",
+        cat_id: ""
+      },
+      () => {
+        console.log(this.state);
+      }
+    );
+  };
+
   render() {
-    const {amount, date, cat_id} = this.state;
+    const { amount, date, cat_id } = this.state;
     return (
-      <div>
-        <div className="container">
-        <div className='flex-container'>   
-            <Logo/>
-            <TransactionDateInput 
-              handleTransactionDate={this.handleTransactionDate}
-              amount={amount}
-              date={date}/>
-            <Select 
-              handleCatId={this.handleCatId}
-              selectedCategoryId={cat_id}/>
+      <div className="container">
+        <div className="flex-container">
+          <Logo />
+          <TransactionDateInput
+            handleTransactionDate={this.handleTransactionDate}
+            amount={amount}
+            date={date}
+          />
+          <Select handleCatId={this.handleCatId} selectedCategoryId={cat_id} />
         </div>
-        <button type="submit" onClick={this.handleSubmit}>Submit</button>
-    </div>
-        
+        <button type="submit" onClick={this.handleSubmit}>
+          Submit
+        </button>
       </div>
-    )
+    );
   }
 }
 
-export default Form
+export default Form;
--- src/Components/Select.js
+++ src/Components/Select.js
@@ -1,133 +1,134 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 // const HOST = 'http://localhost';
-const HOST = 'http://terimorgan.com';
- 
-const API_SAVE_CATEGORY = HOST + '/api/contact/divvy-app/saveCategory.php';
-const API_LOAD_CATEGORIES = HOST + '/api/contact/divvy-app/loadCategories.php';
+const HOST = "http://terimorgan.com";
+
+const API_SAVE_CATEGORY = HOST + "/api/contact/divvy-app/saveCategory.php";
+const API_LOAD_CATEGORIES = HOST + "/api/contact/divvy-app/loadCategories.php";
 
 class Select extends Component {
-    constructor(props) {
-      super(props)   
-      this.state = {
-        userInput: '',
-        categories: [],
-        error: null,
-        categorySaved: false,
-        selectedCategoryId: ''
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInput: "",
+      categories: [],
+      error: null,
+      categorySaved: false
+    };
+  }
+
+  userEnterCategory = event => {
+    this.setState({
+      userInput: event.target.value
+    });
+  };
+
+  saveCategoryInDatabase = () => {
+    axios({
+      method: "post",
+      url: `${API_SAVE_CATEGORY}`,
+      headers: { "content-type": "application/json" },
+      data: this.state
+    })
+      .then(result => {
+        const {
+          data: { newId }
+        } = result;
+        this.updateCategoriesState(newId);
+      })
+      .catch(ex => {
+        alert("The category cannot be saved.");
+        console.warn(ex);
+      });
+  };
+
+  updateCategoriesState = newId => {
+    const { categories, userInput } = this.state;
+    if (newId !== -1) {
+      categories.push({ id: newId, name: userInput });
+    } else {
+      alert("There was a problem retrieving the id from the database");
     }
 
-    userEnterCategory = event => {
-        this.setState({
-            userInput: event.target.value,
-        })}
-
-    saveCategoryInDatabase = () => {
-        axios({
-          method: 'post',
-          url: `${API_SAVE_CATEGORY}`,
-          headers: {'content-type': 'application/json'},
-          data: this.state
-        })
-        .then(result => {                                        
-            const {data: {newId}} = result;
-            this.updateCategoriesState(newId);
-        })
-        .catch(ex => {
-            alert("The category cannot be saved.");
-            console.warn(ex);
-        });
-      }
-
-    updateCategoriesState = (newId) => {
-        const {categories, userInput} = this.state;
-        if (newId !== -1) {
-            categories.push({ id: newId, name: userInput });
-        }
-        else {
-            alert("There was a problem retrieving the id from the database");
-        }
-
-        this.setState({
-            categories,              
-            userInput: '',
-            selectedCategoryId: newId
-        }, () => {
-            console.log(this.state);
-            this.updateFormState();
-        })}
-
-    handleCreateCategory = event => {
-        const { categories, userInput } = this.state;
-        const userInputLC = userInput.toLowerCase();
-
-        //  Compare userInput to each array element
-        for (let i = 0; i < categories.length; i++) {
-            const categoryNameLC = categories[i].name.toLowerCase();
-            if (categoryNameLC === userInputLC) {
-                alert("Category already exists"); 
-                return; 
-            }
-        }  
-        // Add userInput to array only if it's a new category
-        this.saveCategoryInDatabase();
-    }
-
-    loadCategories() {
-        axios({
-            method: 'get',
-            url: `${API_LOAD_CATEGORIES}`
-          })
-          .then(result => {
-            const {categories} = result.data;
-            this.setState({
-              categories
-            })
-          })
-    }
-    
-    componentDidMount() {
-        this.loadCategories();
-    }
-    
-    onSelectCategoryChange = event => {      
-        this.setState({
-            selectedCategoryId: event.target.value
-        }, () => {
-            console.log(this.state);
-            this.updateFormState();
-    })}
-
-    updateFormState() {
+    this.setState(
+      {
+        categories,
+        userInput: ""
+      },
+      () => {
         console.log(this.state);
-        this.props.handleCatId(this.state);
+        this.props.handleCatId(newId);
+      }
+    );
+  };
+
+  handleCreateCategory = event => {
+    const { categories, userInput } = this.state;
+    const userInputLC = userInput.toLowerCase();
+
+    //  Compare userInput to each array element
+    for (let i = 0; i < categories.length; i++) {
+      const categoryNameLC = categories[i].name.toLowerCase();
+      if (categoryNameLC === userInputLC) {
+        alert("Category already exists");
+        return;
+      }
     }
+    // Add userInput to array only if it's a new category
+    this.saveCategoryInDatabase();
+  };
+
+  loadCategories() {
+    axios({
+      method: "get",
+      url: `${API_LOAD_CATEGORIES}`
+    }).then(result => {
+      const { categories } = result.data;
+      this.setState({
+        categories
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.loadCategories();
+  }
+
+  onSelectCategoryChange = event => {
+    const selectedCategoryId = event.target.value;
+    this.props.handleCatId(selectedCategoryId);
+  };
 
   render() {
-    const {userInput, categories, selectedCategoryId} = this.state;
+    const { userInput, categories } = this.state;
+    const { selectedCategoryId } = this.props;
     return (
-      <div className='sub-container'>
+      <div className="sub-container">
         <label>Category:</label>
         <div className="row-container">
-            <input 
-                type="text" 
-                placeholder="Create a category"
-                value={userInput} 
-                onChange={this.userEnterCategory}>
-            </input>
-            <button onClick={this.handleCreateCategory}>Create</button>
+          <input
+            type="text"
+            placeholder="Create a category"
+            value={userInput}
+            onChange={this.userEnterCategory}
+          />
+          <button onClick={this.handleCreateCategory}>Create</button>
         </div>
-        <select value={selectedCategoryId} onChange={this.onSelectCategoryChange}>
-            <option value="">Select a category</option>
-            {categories.map((category) => (
-                <option value={category.id} key={category.id}>{category.name}</option>
-            ))}
+        <select
+          value={selectedCategoryId}
+          onChange={this.onSelectCategoryChange}
+        >
+          <option value="">Select a category</option>
+          {categories.map(category => (
+            <option value={category.id} key={category.id}>
+              {category.name}
+            </option>
+          ))}
         </select>
       </div>
-    )
+    );
   }
 }
 
-export default Select
+export default Select;
